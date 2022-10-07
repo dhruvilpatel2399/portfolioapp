ec5cfef6faabcda481c8704dde73f5b88412e1e7
dhruvil patel <dp3097@gmail.com>
2022-10-06 21:17:59 -0400

    Routing :(page)

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+/* GET Products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET About Us page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
 module.exports = router;
